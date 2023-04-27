c1262a11ff5012b99e042c73c7f58f60b4c42f00
ZhekaProff <Zhekakad@ya.ru>
2023-04-27 17:03:11 +0900

    решение DZ4 исправленное

--- src/DZ4/Task02_my.sql
+++ src/DZ4/Task02_my.sql
@@ -3,23 +3,23 @@ CREATE DATABASE People;
 USE People;
 
 CREATE TABLE People(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 name VARCHAR(50) NOT NULL,
 age INT NOT NULL,
 address VARCHAR(100)
 );
-INSERT INTO people (id, name, age, address) VALUES
-(1, 'John', 25, 'Moscow'),
-(2, 'Emily', 30, 'Moscow'),
-(3, 'David', 42, 'Moscow'),
-(4, 'Sara', 19, 'Moscow'),
-(5, 'Michael', 56, '1111 Maple Ln'),
-(6, 'Laura', 32, '1313 Cedar Blvd'),
-(7, 'Peter', 48, '1414 Spruce St'),
-(8, 'Karen', 21, '1515 Birch Pl'),
-(9, 'Alex', 39, 'Moscow'),
-(10, 'Nicholas', 27, '1717 Grove Rd');
+INSERT INTO people (name, age, address) VALUES
+('John', 25, 'Moscow'),
+('Emily', 30, 'Moscow'),
+('David', 42, 'Moscow'),
+('Sara', 19, 'Moscow'),
+('Michael', 56, '1111 Maple Ln'),
+('Laura', 32, '1313 Cedar Blvd'),
+('Peter', 48, '1414 Spruce St'),
+('Karen', 21, '1515 Birch Pl'),
+('Alex', 39, 'Moscow'),
+('Nicholas', 27, '1717 Grove Rd');
 
-SELECT name 
+SELECT id, name 
 FROM People
 WHERE address = 'Moscow' AND age >= 18 AND age < 30;
